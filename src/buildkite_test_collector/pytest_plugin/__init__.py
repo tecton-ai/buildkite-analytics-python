5dc85a30aae67c17afe3ff59a5917f4884883fa8
Brian Hart <brian@tecton.ai>
2023-01-12 00:05:57 -0800

    Add option to write analytics to a specified json output file

--- src/buildkite_test_collector/pytest_plugin/__init__.py
+++ src/buildkite_test_collector/pytest_plugin/__init__.py
@@ -1,5 +1,6 @@
 """Buildkite test collector for Pytest."""
 
+import json
 from logging import warning
 
 import pytest
@@ -20,6 +21,14 @@ def spans(request):
     return SpanCollector(plugin=plugin, nodeid=nodeid)
 
 
+@pytest.hookimpl
+def pytest_addoption(parser, pluginmanager):
+    group = parser.getgroup("buildkite-test-collector")
+    group.addoption(
+        "--buildkite-analytics-json-out", action="store", help="JSON file to write analytics payload to"
+    )
+
+
 @pytest.hookimpl(trylast=True)
 def pytest_configure(config):
     """pytest_configure hook callback"""
@@ -38,4 +47,8 @@ def pytest_unconfigure(config):
     plugin = config.pluginmanager.get_plugin("_buildkite")
     if plugin:
         submit(plugin.payload)
+
+        if config.getoption("--buildkite-analytics-json-out"):
+            with open(config.getoption("--buildkite-analytics-json-out"), mode="w") as f:
+                f.write(json.dumps(plugin.payload.as_json()))
         config.pluginmanager.unregister(plugin)
